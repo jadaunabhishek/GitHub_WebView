e8cc28952ebcdadb96e045251a5348a9e14d74dd
rajput_asj <ajadaun883@gmail.com>
2023-07-01 00:39:02 +0530

    Initial commit

---
+++ app/src/main/java/com/abhidev/webview/MainActivity.java
@@ -0,0 +1,56 @@
+package com.abhidev.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.weblayout);
+        progressBar = findViewById(R.id.progresslayout);
+
+        // load first page
+        webView.loadUrl("https://github.com/jadaunabhishek");
+
+        // client, so overall web open in our app
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                progressBar.setVisibility(View.VISIBLE);
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                progressBar.setVisibility(View.GONE);
+                super.onPageFinished(view, url);
+            }
+        });
+
+    }
+
+    // to go back in website
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+}
